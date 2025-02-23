0e5a323f71e8068d7de64959f6b580c23299797c
Kallielmpinheiro <Kallielmpinheiro2004@gmail.com>
2025-02-22 22:47:51 -0300

    feat: Implementando endpoint para teste

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .schemas import TestReq, TestResp
+from api.api import test_endpoint
 
 # Create your tests here.
+
+class TestEndpointTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_valid_name(self):
+        data = TestReq(name="Test Name")
+        status_code, response = test_endpoint(None, data)
+        self.assertEqual(status_code, 200)
+        self.assertIsInstance(response, TestResp)
+        self.assertEqual(response.name, "Test Name")
+
+    def test_empty_name(self):
+        data = TestReq(name="")
+        status_code, response = test_endpoint(None, data)
+        self.assertEqual(status_code, 404)
+        self.assertEqual(response, {"error": "Name is required"})
