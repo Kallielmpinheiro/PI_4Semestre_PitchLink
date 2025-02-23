c7d52e0c54cfb936d89b5924997fb2ce56e3468d
Pedro Rufino <pedro.rmn19@gmail.com>
2025-02-23 20:32:19 -0300

    [Style]: componenet home

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'frontend';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
---
+++ frontend/src/app/modules/pitchlink/pages/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+// Components
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
