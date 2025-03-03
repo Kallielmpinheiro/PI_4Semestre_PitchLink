7ac105d4b66ce58e1f64adfcd6ec9a87aa0dabb3
Jonathan Rocha <Jonathansantanarocha27@gmail.com>
2025-03-03 13:45:45 -0300

    Style: criando modal de login

--- frontend/src/app/modules/pitchlink/components/main/main.component.ts
+++ frontend/src/app/modules/pitchlink/components/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ModalLoginComponent } from '../modal-login/modal-login.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [ModalLoginComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
