d155c8116f07e94c1b86348661702b3502fa9385
kallielmpinheiro <kallielmpinheiro2004@gmail.com>
2025-03-04 19:15:13 -0300

    fix: erro no cancelamento

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from allauth.socialaccount.providers.oauth2.views import OAuth2CallbackView
+from allauth.socialaccount.providers.openid_connect.views import OpenIDConnectOAuth2Adapter
+from django.shortcuts import redirect
 
 # Create your views here.
+
+def custom_linkedin_callback(request):
+
+    if request.GET.get('error') == 'user_cancelled_login' or request.GET.get('error') == 'user_cancelled_authorize':
+        return redirect('http://localhost:4200/')
+
+    provider_id = "linkedin-server"
+    adapter = OpenIDConnectOAuth2Adapter(request, provider_id)
+
+    class LinkedInCallbackView(OAuth2CallbackView):
+        adapter_class = OpenIDConnectOAuth2Adapter
+
+    view = LinkedInCallbackView()
+    view.request = request  
+    view.adapter = adapter  
+    return view.dispatch(request)
+
+
