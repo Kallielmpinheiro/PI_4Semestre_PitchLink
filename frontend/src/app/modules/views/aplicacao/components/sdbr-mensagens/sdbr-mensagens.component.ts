d7b9873ac336d6b4aa040ceb83353c23c83f7939
Pedro Rufino <pedro.rufino@nappsolutions.com>
2025-04-09 16:23:16 -0300

    [Feat]: component mensagens

---
+++ frontend/src/app/modules/views/aplicacao/components/sdbr-mensagens/sdbr-mensagens.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sdbr-mensagens',
+  imports: [],
+  templateUrl: './sdbr-mensagens.component.html',
+  styleUrl: './sdbr-mensagens.component.css'
+})
+export class SdbrMensagensComponent {
+
+}
