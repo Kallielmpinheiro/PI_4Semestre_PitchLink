138f5ceae14ea8395533ca184d4c7e4a4363cce6
Pedro Rufino <pedro.rmn19@gmail.com>
2025-06-05 12:54:10 -0300

    [Feat]: create Componenet negocios-fechados

---
+++ frontend/src/app/modules/views/negocios-fechados/negocios-fechados.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-negocios-fechados',
+  imports: [],
+  templateUrl: './negocios-fechados.component.html',
+  styleUrl: './negocios-fechados.component.css'
+})
+export class NegociosFechadosComponent {
+
+}
